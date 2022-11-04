a49cd1420c3d1d6d67a553a88baefc3f7edf1dc1
luca <luca.marignati@edu.unito.it>
2022-11-04 22:15:05 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StyledEngineProvider } from '@mui/material/styles';
-import Demo from "./UI/demo";
+import Demo from './UI/Demo';
 
 function App() {
   return (
