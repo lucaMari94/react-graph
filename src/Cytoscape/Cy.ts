d7d507df438775478ba2d3c38b818fe94e38f628
Luca Marignati <luca.marignati@larus-ba.it>
2022-11-09 09:02:41 +0100

    fix cytoscape

---
+++ src/Cytoscape/Cy.ts
@@ -0,0 +1,76 @@
+import cytoscape from "cytoscape";
+import { Core } from "cytoscape";
+import { MutableRefObject } from "react";
+
+export class Cytoscape {
+    cy:Core;
+   
+    constructor(graphRef: MutableRefObject<null>) {
+        this.cy = cytoscape({
+            container: graphRef.current,
+            elements: [
+              { data: { id: 'gryffindor'} },
+              { data: { id: 'hufflepuff' } },
+              { data: { id: 'ravenclaw' } },
+              { data: { id: 'slytherin' } },
+            ],
+            style: [
+              {
+                selector: 'node',
+                css: {
+                  'label': 'data(id)',
+                  "text-background-opacity": 0.5,
+                  "text-background-color": "white"
+                }
+              },
+              {
+                "selector": ".autorotate",
+                "style": {
+                    "font-size": 10,
+                    "source-text-rotation": "autorotate",
+                    "target-text-rotation": "autorotate",
+                    "source-text-offset": 50,
+                    "target-text-offset": 50,
+                    "text-background-shape":"rectangle",
+                    "text-background-color":"#000000",
+                    "text-background-opacity":1
+                }
+              }
+            ],
+            layout: {
+              name: 'grid',
+            },
+            // initial viewport state:
+            zoom: 0,
+            pan: { x: 0, y: 0 },
+    
+            // interaction options:
+            minZoom: 1e-50,
+            maxZoom: 1e50,
+            zoomingEnabled: true,
+            userZoomingEnabled: true,
+            panningEnabled: true,
+            userPanningEnabled: true,
+            boxSelectionEnabled: true,
+            selectionType: 'single',
+            touchTapThreshold: 8,
+            desktopTapThreshold: 4,
+            autolock: false,
+            autoungrabify: false,
+            autounselectify: false,
+            // multiClickDebounceTime: 250,
+    
+            // rendering options:
+            headless: false,
+            styleEnabled: true,
+            hideEdgesOnViewport: false,
+            textureOnViewport: false,
+            motionBlur: false,
+            motionBlurOpacity: 0.2,
+            wheelSensitivity: 1,
+            pixelRatio: 'auto'
+          })
+    }
+   
+    
+}
--- src/UI/GraphRender.tsx
+++ src/UI/GraphRender.tsx
@@ -1,85 +1,22 @@
-import cytoscape, { Core, EventObject } from "cytoscape"
+import { EventObject } from "cytoscape"
 import { FC, Fragment, useCallback, useEffect, useRef } from "react"
+import { Cytoscape } from "../Cytoscape/Cy";
 
 interface GraphRenderProps{}
 
 const GraphRender:FC<GraphRenderProps> = (props:GraphRenderProps) => {
 
-    let cy:Core;
+    let cytoscape:any;
 
     const graphRef = useRef(null)
    
     const drawGraph = () => {
-      cy = cytoscape({
-        container: graphRef.current,
-        elements: [
-          { data: { id: 'gryffindor'} },
-          { data: { id: 'hufflepuff' } },
-          { data: { id: 'ravenclaw' } },
-          { data: { id: 'slytherin' } },
-        ],
-        style: [
-          {
-            selector: 'node',
-            css: {
-              'label': 'data(id)',
-              "text-background-opacity": 0.5,
-              "text-background-color": "white"
-            }
-          },
-          {
-            "selector": ".autorotate",
-            "style": {
-                "font-size": 10,
-                "source-text-rotation": "autorotate",
-                "target-text-rotation": "autorotate",
-                "source-text-offset": 50,
-                "target-text-offset": 50,
-                "text-background-shape":"rectangle",
-                "text-background-color":"#000000",
-                "text-background-opacity":1
-            }
-          }
-        ],
-
-        layout: {
-          name: 'cose',
-        },
-        // initial viewport state:
-        zoom: 1,
-        pan: { x: 0, y: 0 },
-
-        // interaction options:
-        minZoom: 1e-50,
-        maxZoom: 1e50,
-        zoomingEnabled: true,
-        userZoomingEnabled: true,
-        panningEnabled: true,
-        userPanningEnabled: true,
-        boxSelectionEnabled: true,
-        selectionType: 'single',
-        touchTapThreshold: 8,
-        desktopTapThreshold: 4,
-        autolock: false,
-        autoungrabify: false,
-        autounselectify: false,
-        // multiClickDebounceTime: 250,
-
-        // rendering options:
-        headless: false,
-        styleEnabled: true,
-        hideEdgesOnViewport: false,
-        textureOnViewport: false,
-        motionBlur: false,
-        motionBlurOpacity: 0.2,
-        wheelSensitivity: 1,
-        pixelRatio: 'auto'
-      })
-      cy.layout({name: 'cose'}).run();
-      cy.on('tap', 'node', function(evt){
-        // var node = evt.target;
-        // console.log( 'tapped ' + node.id() );
-        nodeClickHandler(evt);
+      cytoscape = new Cytoscape(graphRef);
+      // cytoscape.cy.layout({name: 'grid'}).run();
+      cytoscape.cy.on('tap', 'node', function(evt: EventObject){
+          // var node = evt.target;
+          // console.log( 'tapped ' + node.id() );
+          nodeClickHandler(evt);
       });
     }
     
@@ -99,11 +36,11 @@ const GraphRender:FC<GraphRenderProps> = (props:GraphRenderProps) => {
         .then((res)=>{
           console.log(res);
           res.forEach((element: any, index: number) => {
-            cy.add([
+            cytoscape.cy.add([
               {group: 'nodes',data: { id: element.name },}, 
               {group: 'edges', data: { id: index.toString(), source: evt.target.id(), target: element.name }, classes:'autorotate' }
             ]);
-            cy.layout({name: 'cose'}).run();
+            cytoscape.cy.layout({name: 'cose'}).run();
         });
         })
         .catch((error) => {
@@ -112,7 +49,7 @@ const GraphRender:FC<GraphRenderProps> = (props:GraphRenderProps) => {
     };
     
     useEffect(() => {
-     // drawGraph()
+      drawGraph()
     }, [])
    
     return (
